b9252fd4fc3156bb7b636e26c67bf1e32c8bd707
Wim Zwart <wwzwart@gmail.com>
2020-02-02 13:10:51 +0000

    Class definition

---
+++ AV_CW.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import struct
+from vpython import *
+
+class THREEDHEAD():
+    def __init__(self):
+        self.data_path=None
+        self.data_path=None
+
+    def read_from_file(self, sequence_id, frame_id):
+        self.sequence_id=sequence_id
+        self.frame_id=frame_id
+
+
+    def img_coord_from_xyz(self):
+
+        # creates a list of x,y coordinates
+
+        self.index2xy=None
+
+
+    def filter_1(self):
+        self.rgb=self.rgb
+        self.xyz=self.xyz
+        self.img_bw=self.img_bw
+
+
+    def remove_edges(self):
+
+    def somefilter(self):
+
