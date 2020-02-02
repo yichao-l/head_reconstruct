341112fe1844aa2b1cb1ea741f225c30a4f887a3
Wim Zwart <wwzwart@gmail.com>
2020-02-02 21:15:30 +0000

    Added Comments and filtered 2d images

--- HEAD_RECON.py
+++ HEAD_RECON.py
@@ -14,7 +14,6 @@ class threeD_head():
     def __init__(self, data_path=None):
         self.data_path=data_path
 
-
     @classmethod
     def read_from_file(cls, sequence_id, frame_id):
         '''
@@ -37,16 +36,24 @@ class threeD_head():
         this.rgb = np.asarray([float_2_rgb(num) for num in pc[:, 3]])/256
         this.sequence_id=sequence_id
         this.frame_id=frame_id
+        this.img_coord_from_xyz()
         return this
 
     @classmethod
     def load(cls, data_file='head.p'):
-        with open('head.p', 'rb') as file_object:
-            raw_data = file_object.read()
-        return pickle.loads(raw_data)
+        '''
+        :param data_file:  file to load from, default name is the default file used for saving
+        :return: object of  threeD_head class
+        '''
+        try:
+            with open('head.p', 'rb') as file_object:
+                raw_data = file_object.read()
+            return pickle.loads(raw_data)
+        except:
+            raise FileExistsError (f'{data_file} could not be found, create {data_file} by using .save() first ')
 
 
-    def img_coord_from_xyz(self, rgb):
+    def img_coord_from_xyz(self):
         '''
         Convert a list of rbg values to a 2D image
         params:
@@ -54,9 +61,33 @@ class threeD_head():
         return:
             the rgb image in a 480, 640 format
         '''
-        image = np.reshape(this.rgb,(480,640,3))      
+        image = np.reshape(self.rgb,(480,640,3))
+        self.xy_mesh=np.arange(640*480)
         self.twoD_image= image
         return image
+
+    def get_filtered_image(self):
+        '''
+        get the 2d image after all filters have been applied
+        :return: returns image filterd by all filter operations, black where filters have been applied
+        '''
+        twoD_image= self.twoD_image.copy().reshape(-1, 3)
+        img= np.zeros((480*640,3))
+        for v in self.xy_mesh:
+            img[v]=twoD_image[v]
+        return img.reshape(480,640,3)
+
+
+    def get_bw_image(self):
+        '''
+        :return: black and white image with all pixels set to the filtered colour
+        '''
+
+        img= np.zeros((480*640,3))
+        for v in self.xy_mesh:
+            img[v]=[1,1,1]
+        return img.reshape(480,640,3)
+
 
     def filter_1(self):
         self.rgb=self.rgb
@@ -65,35 +96,61 @@ class threeD_head():
 
     def filter_depth(self,depth):
         '''
-        Keep the ... 
+        :param depth: any pixel with depth greater than this value is removed
+        :return:
         '''
         depth_filter = self.xyz[:, 2] < depth
+        self.xy_mesh=self.xy_mesh[depth_filter]
         self.rgb=  self.rgb[depth_filter]
         self.xyz=  self.xyz[depth_filter]
 
+
     def filter_nan(self):
+        '''
+        removes all entries where any of the xyz coordinates is nan
+        '''
         nan_filter = ~np.isnan(self.xyz).any(axis=1)
+        self.xy_mesh=self.xy_mesh[nan_filter]
         self.xyz = self.xyz[nan_filter]
         self.rgb = self.rgb[nan_filter]
 
     def sparsify(self,sparsity):
+        '''
+
+        :param sparsity: the fraction of pixles that is retained
+        :return: updates the object
+        '''
         l=self.xyz.shape[0]
         filter = np.random.random((l)) < sparsity
+        self.xy_mesh=self.xy_mesh[filter]
         self.xyz = self.xyz[filter]
         self.rgb = self.rgb[filter]
 
     def center(self):
+        '''
+        centers the object
+        :return:
+        '''
         self.xyz = self.xyz - self.xyz.mean(axis=0)
 
     def remove_edges(self):
         return None
 
     def create_vpython_spheres(self):
+        '''
+        creates the spheres that can be used by vpython
+        :return:
+        '''
         self.spheres = []
         for i in range(self.xyz.shape[0]):
             next = vec(self.xyz[i,0],-self.xyz[i,1],-self.xyz[i,2])
             self.spheres.append({'pos':next, 'radius':0.003, 'color':(vec(self.rgb[i,0],self.rgb[i,1],self.rgb[i,2]))})
 
     def save(self, file_name='head.p'):
+        '''
+
+        :param file_name:
+        :return:
+        '''
         pickle.dump(self, open(file_name, 'wb'))
 
--- gui.py
+++ gui.py
@@ -1,3 +1,7 @@
+'''
+GUI for rendering 3D head objects
+'''
+
 import HEAD_RECON
 import numpy as np
 import matplotlib.pyplot as plt
