08296eddc2ce6c4ef0649436983c63639b2ff38e
Wim Zwart <wwzwart@gmail.com>
2020-02-09 14:06:02 +0000

    No images during search

--- SIFT.py
+++ SIFT.py
@@ -18,7 +18,7 @@ def get_descriptors(img_path,SIFT_contrastThreshold=0.04,SIFT_edgeThreshold=10,S
     kp, des = sift.detectAndCompute(img,None)
     return kp, des
 
-def get_matched_points(img1, kp1, des1, img2, kp2, des2, ratio=0.7):
+def get_matched_points(img1, kp1, des1, img2, kp2, des2, ratio=0.7, searching=True):
     '''
     find a set of good matching descriptors given two set of keypoints and descriptors
     '''
@@ -26,24 +26,25 @@ def get_matched_points(img1, kp1, des1, img2, kp2, des2, ratio=0.7):
     img2 = cv2.imread(img2)
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
-    cv2.imwrite("img1.png",img1)
+    cv2.imwrite("img1.png", img1)
     # Apply ratio test
     good = []
     good_without_list = []
-    for m,n in matches:
-        if m.distance < ratio*n.distance:
+    for m, n in matches:
+        if m.distance < ratio * n.distance:
             good.append([m])
             good_without_list.append(m)
 
     # cv2.drawMatchesKnn expects list of lists as matches.
-    img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+    if not searching:
+        img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
 
-    plt.imshow(img3),plt.show()
-    cv2.imwrite("des_match.png",img3)
+        plt.imshow(img3), plt.show()
+        cv2.imwrite("des_match.png", img3)
     return good_without_list
 
 
-def clean_matches(kp1, img1, kp2, img2, matches, min_match=8):
+def clean_matches(kp1, img1, kp2, img2, matches, min_match=8, searching=True):
     '''
     param:
         matches (list(DMatch)): a list of matching object
@@ -56,34 +57,35 @@ def clean_matches(kp1, img1, kp2, img2, matches, min_match=8):
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
     MIN_MATCH_COUNT = min_match
     if len(matches)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
 
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,10)
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10)
         matchesMask = mask.ravel().tolist()
 
-        h,w = img1.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
-
-        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        h, w = img1.shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, M)
+        if not searching:
+            img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 
     else:
         print("Not enough matches are found - %d/%d, turn up the match ratio." % (len(matches), MIN_MATCH_COUNT))
         matchesMask = None
 
     mask = np.array(matchesMask)
-    mask = [m>0 for m in mask.reshape([-1,])]
+    mask = [m > 0 for m in mask.reshape([-1, ])]
     cleaned_matches = np.array(matches)[mask]
 
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                singlePointColor = None,
-                matchesMask = matchesMask, # draw only inliers
-                flags = 2)
+    if not searching:
+        draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                           singlePointColor=None,
+                           matchesMask=matchesMask,  # draw only inliers
+                           flags=2)
 
-    img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None,**draw_params)
-    cv2.imwrite("des_match_cleaned.png",img3)
-    plt.imshow(img3, 'gray'),plt.show()
+        img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, **draw_params)
+        cv2.imwrite("des_match_cleaned.png", img3)
+        plt.imshow(img3, 'gray'), plt.show()
 
     return cleaned_matches
 
--- multi_head.py
+++ multi_head.py
@@ -32,17 +32,18 @@ class MultiHead():
         self.heads.append(head)
 
 
-    def join_heads(self, index1, index2, SIFT_contrastThreshold=0.02,SIFT_edgeThreshold=10,SIFT_sigma = 1,searching=1):
+    def join_heads(self, index1, index2, SIFT_contrastThreshold=0.02, SIFT_edgeThreshold=10, SIFT_sigma=1,
+                   searching=True):
         head1 = self.heads[index1]
         head2 = self.heads[index2]
 
         img1, path1 = head1.get_filtered_image()
         img2, path2 = head2.get_filtered_image()
-        kp1, des1 = get_descriptors(path1,SIFT_contrastThreshold,SIFT_edgeThreshold,SIFT_sigma)
-        kp2, des2 = get_descriptors(path2,SIFT_contrastThreshold,SIFT_edgeThreshold,SIFT_sigma)
-        good_without_list = get_matched_points(path1, kp1, des1, path2, kp2, des2, 1)
+        kp1, des1 = get_descriptors(path1, SIFT_contrastThreshold, SIFT_edgeThreshold, SIFT_sigma)
+        kp2, des2 = get_descriptors(path2, SIFT_contrastThreshold, SIFT_edgeThreshold, SIFT_sigma)
+        good_without_list = get_matched_points(path1, kp1, des1, path2, kp2, des2, 1, searching=searching)
 
-        cleaned_match = clean_matches(kp1, path1, kp2, path2, good_without_list)
+        cleaned_match = clean_matches(kp1, path1, kp2, path2, good_without_list, searching=searching)
 
         # head1.reset_colors()
         # head2.reset_colors()
@@ -123,13 +124,12 @@ class MultiHead():
         # self.spheres = head1.spheres + head2.spheres
 
         # comment out for speed
-        head1.create_vpython_spheres()
-        head2.create_vpython_spheres()
-
-        self.spheres = head1.spheres + head2.spheres
-
-        self.create_spheres()
-        self.save()
+        if not searching:
+            head1.create_vpython_spheres()
+            head2.create_vpython_spheres()
+            self.spheres = head1.spheres + head2.spheres
+            self.create_spheres()
+            self.save()
         return True
     
     def get_mean_distance(self,index1,index2,r=0.01,distance_threshold=0.04):
@@ -172,13 +172,13 @@ class MultiHead():
 
         for con_thresh,edge_thresh,sigma in params:
                 try: # catch bad parameters
-                    distance = self.join_heads(index1,index2,con_thresh,edge_thresh,sigma,searching=1)
+                    distance = self.join_heads(index1, index2, con_thresh, edge_thresh, sigma, searching=True)
                 except:
                     distance = 100000
                 distances.append(distance)
         min_idx = np.argmin(distances)
-        print(min_idx,"min_dist",distances[min_idx],"params:",params[min_idx])
-        self.join_heads(index1,index2,params[min_idx][0],params[min_idx][1],params[min_idx][2],searching=0)
+        print(min_idx, "min_dist", distances[min_idx], "params:", params[min_idx])
+        self.join_heads(index1, index2, params[min_idx][0], params[min_idx][1], params[min_idx][2], searching=False)
         return
 
 
