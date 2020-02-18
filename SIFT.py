96baaf87dc3b01526fedee0ada070bfcea17d047
Wim Zwart <wwzwart@gmail.com>
2020-02-18 15:54:56 +0000

    All Heads together

--- SIFT.py
+++ SIFT.py
@@ -5,33 +5,39 @@ from tqdm import tqdm_notebook as tqdm
 from Procrustes2 import *
 
 
-def get_matches(head1, head2, SIFT_contrastThreshold=0.04, SIFT_edgeThreshold=10, SIFT_sigma=4):
-    img1 = head1.get_filtered_image()
-    img2 = head2.get_filtered_image()
-    kp1, des1 = get_descriptors(img1, SIFT_contrastThreshold, SIFT_edgeThreshold, SIFT_sigma)
-    kp2, des2 = get_descriptors(img2, SIFT_contrastThreshold, SIFT_edgeThreshold, SIFT_sigma)
-    # remove the edges that are on the edge
-    rad = 5  # minimum distance from the edge
-    kp1, des1 = head1.remove_edge_points(kp1, des1, rad=rad)
-    kp2, des2 = head2.remove_edge_points(kp2, des2, rad=rad)
+def get_matches(head1, head2):
     # find K nearest matches ( in terms of descriptors
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    matches = bf.knnMatch(head1.des, head2.des, k=2)
     # unfoled the list
     matches = [val for sublist in matches for val in sublist]
-    return kp1, kp2, matches
+    return matches
 
 
-def ransac(head1, head2, kp1, kp2, matches):
+def remove_height_variation_from_matches(head1, head2, matches):
     max_variation_y_dimension = 15
-    matches = [m for m in matches if (abs(kp1[m.queryIdx].pt[1] - kp2[m.trainIdx].pt[1]) < max_variation_y_dimension)]
-    pts1 = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    matches = [m for m in matches if
+               (abs(head1.kp[m.queryIdx].pt[1] - head2.kp[m.trainIdx].pt[1]) < max_variation_y_dimension)]
+    return matches
+
+
+def get_xyz_from_matches(head1, head2, matches):
+    '''
+
+    :param head1: a head object
+    :param head2: a head object
+    :param matches: amacthes object
+    :return: xyz1/2 the sets of 3d points of the respective heads, corresponding to the matches
+    '''
+
+    matches = remove_height_variation_from_matches(head1, head2, matches)
+
+    pts1 = np.float32([head1.kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
     xy1 = np.round(pts1).astype("int").reshape(-1, 2)
     xyindex1 = xy1[:, 1] * 640 + xy1[:, 0]
     indices1 = [np.argwhere(head1.xy_mesh == ind) for ind in xyindex1]
     filter1 = [len(ind) > 0 for ind in indices1]
-    xyz1 = head1.xyz_unfiltered[xyindex1]
-    pts2 = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    pts2 = np.float32([head2.kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
     xy2 = np.round(pts2).astype("int").reshape(-1, 2)
     xyindex2 = xy2[:, 1] * 640 + xy2[:, 0]
     indices2 = [np.argwhere(head2.xy_mesh == ind) for ind in xyindex2]
@@ -41,19 +47,24 @@ def ransac(head1, head2, kp1, kp2, matches):
     indices2 = np.asarray(indices2)[filter]
     xyz1 = np.asarray([head1.xyz[ind[0][0]] for ind in indices1])
     xyz2 = np.asarray([head2.xyz[ind[0][0]] for ind in indices2])
+    return xyz1, xyz2, matches
+
+
+def ransac(head1, head2, matches):
+    xyz1, xyz2, matches = get_xyz_from_matches(head1, head2, matches)
+
     max_dist = 0.015
     best_count = 0
     best_err = 1000
     best_inliers = []
-    No_Iterations = 500
+    No_Iterations = 5000
     min_num_inliers = 6
-    print(xyz2.shape[0])
+
     with tqdm(total=No_Iterations) as progressbar:
         for j in range(No_Iterations):
             inliers = np.random.rand(xyz2.shape[0]) > 0.5
             progressbar.update(1)
             for i in range(20):
-
                 if np.sum(inliers) >= min_num_inliers:
                     try:
                         d, Z, tform = procrustes(xyz1[inliers], xyz2[inliers], scaling=False, reflection='best')
@@ -65,7 +76,8 @@ def ransac(head1, head2, kp1, kp2, matches):
                         if (np.sum(inliers) >= best_count):
                             # if (err < best_err) and np.sum(inliers)>min_num_inliers:
                             best_err = err
-                            progressbar.set_description(f"Best Count {best_count:.0f} Error {err:.4f}")
+                            progressbar.set_description(
+                                f"Head {head1.frame_id} & {head2.frame_id} :best count {best_count:.0f} Error {err:.4f}")
                             best_count = np.sum(inliers)
                             best_err = err
                             best_inliers = inliers.copy()
@@ -76,23 +88,20 @@ def ransac(head1, head2, kp1, kp2, matches):
                         pass
                 else:
                     break
-    head1.keypoints = xyz1[best_inliers]
-    head1.keypoints_clr = [1, 0, 0]
-    head2.keypoints = xyz2[best_inliers]
-    head2.keypoints_clr = [0, 1, 0]
+
     if best_count < 6:
         raise ValueError('Could not match')
     return best_tform, best_inliers, best_err, matches
 
 
-def draw_matches(head1, head2, kp1, kp2, matches, inliers):
+def draw_matches(head1, head2, matches, inliers):
     draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
                        singlePointColor=None, flags=2)
 
     img1 = cv2.cvtColor((head1.get_filtered_image() * 256).astype("uint8"), cv2.COLOR_BGR2RGB)
     img2 = cv2.cvtColor((head2.get_filtered_image() * 256).astype("uint8"), cv2.COLOR_BGR2RGB)
 
-    img3 = cv2.drawMatches(img1, kp1, img2, kp2, [match for i, match in enumerate(matches) if inliers[i]],
+    img3 = cv2.drawMatches(img1, head1.kp, img2, head2.kp, [match for i, match in enumerate(matches) if inliers[i]],
                            None, **draw_params)
     cv2.imwrite("des_match_cleaned.png", img3)
 
