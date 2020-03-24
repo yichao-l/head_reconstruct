25f8a62e96f91e1a831ff722437df31a3eb1e5a8
Wim Zwart <wwzwart@gmail.com>
2020-03-24 17:11:52 +0000

    options for SIFT Transform

--- av_cw.py
+++ av_cw.py
@@ -5,6 +5,7 @@ Produces images of the individual merging steps and calculates performance metri
 '''
 from single_head import SingleHead
 from multi_head import MultiHead
+import numpy as np
 
 # Seqence number in the data, range from 1 to 4.
 Sequence = 1
@@ -30,15 +31,28 @@ mhead.calc_all_sift_keypoints()
 # calculate the SIFT transform for each pair of adjacent heads, each pair of adjacnt SingleHeads shares a Link Object:
 mhead.calc_all_sift_transforms()
 
-Method A
+# Method A
 for link_idx in range(14):  # iterate through the links between heads
     # calculate and perform all transformations for each link:
     mhead.all_transforms_from_link(mhead.links[link_idx])
-    
+
 # # Method C
-# for _ in range(14):
-#     best_link_index, err = mhead.get_next_unpositioned_link()
-#     mhead.transform_from_link(mhead.links[best_link_index])
+only_first_n = 15
+mhead.reset_all_head_positions()
+link_index, err = mhead.get_next_unpositioned_link(method="coverage")
+positioned_head_count = 0
+joined_heads = set()
+while (not link_index is None) and (positioned_head_count < only_first_n or only_first_n == -1):
+    mhead.links[link_index].print_short()
+    joined_heads.add(mhead.links[link_index].left)
+    joined_heads.add(mhead.links[link_index].right)
+    mhead.heads[mhead.links[link_index].right - 1].background_color = np.asarray([0, 0, 1])
+    mhead.heads[mhead.links[link_index].left - 1].background_color = np.asarray([0, 0, 1])
+    foo = mhead.all_transforms_from_link(mhead.links[link_index], method="coverage", ICP=True, Refine_Range=False,
+                                         Refine_local=False)
+    link_index, err = mhead.get_next_unpositioned_link(method="coverage")
+    positioned_head_count = max(positioned_head_count + 1, 2)
+mhead.left_eye_deviation()
 
 # create a series of png images for the spheres
 mhead.create_png_series()
